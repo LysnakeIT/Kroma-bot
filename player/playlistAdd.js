da9dc8ac45005fc6dc4943682fa4380c3a6cdc75
Lilian <73485512+LysnakeIT@users.noreply.github.com>
2021-11-10 12:47:23 +0100

    Delete player directory

--- player/playlistAdd.js
+++
@@ -1,3 +0,0 @@
-module.exports = (client, message, queue, playlist) => {
-    message.channel.send(`${client.emotes.music} - ${playlist.title} a été ajouté à la file d'attente (**${playlist.tracks.length}** musiques) !`);
-};
--- player/trackAdd.js
+++
@@ -1,3 +0,0 @@
-module.exports = (client, message, queue, track) => {
-    message.channel.send(`${client.emotes.music} - ${track.title} a été ajouté à la file d'attente !`);
-};
