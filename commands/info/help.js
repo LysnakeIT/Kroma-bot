4e69a30825d43cbec97af58b08d385ffe593c875
Lilian <73485512+LysnakeIT@users.noreply.github.com>
2021-11-14 16:12:02 +0100

    Ajout des commandes de musique

--- commands/info/help.js
+++ commands/info/help.js
@@ -9,7 +9,7 @@ module.exports = {
         const help_embed = new Discord.MessageEmbed()
             .setColor("#00FF04")
             .setTitle("**Kroma Help**")
-            .setDescription("**Préfixe :** `!`\n\n :earth_africa: Liste des commandes **Servers** : \n\n `!serverinfos` `!info` `!covid` `!math` `!ping` `!system` `!yt`\n\n :desktop: Commande **Hardware/Software** : \n\n`!hs`\n\n :video_game: Liste des commandes **Games** : \n\n `!pendu` `!quizz`")
+            .setDescription("**Préfixe :** `!`\n\n :earth_africa: Liste des commandes **Servers** : \n\n `!serverinfos` `!info` `!covid` `!math` `!ping` `!weather` `!translate` `!system` `!yt`\n\n :desktop: Commande **Hardware/Software** : \n\n`!hs`\n\n :video_game: Liste des commandes **Games** : \n\n `!quizz`\n\n :headphones: Liste des commandes **Musique** : `!clear-queue` `!loop` `!play` `!queue` `!search` `!skip` `!stop`")
         message.channel.send({ embeds : [help_embed] })
     }
 }
