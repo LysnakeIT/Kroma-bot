6d5821894a2c806601f79e779613d0fe589fa899
Lilian <73485512+LysnakeIT@users.noreply.github.com>
2022-09-06 23:14:28 +0200

    🚀Mise à jour vers la v14 de discord.js 3ème PARTIE

--- config/bot.js
+++ config/bot.js
@@ -6,28 +6,10 @@ module.exports = {
         music: ':musical_note:',
         success: ':white_check_mark:',
     },
-
     discord: {
         token: 'process.env.token',
         prefix: '!',
     },
-    
-    opt: {
-        DJ: {
-            enabled: false,
-            roleName: 'DJ',
-            commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'seek', 'shuffle', 'skip', 'stop', 'volume']
-        },
-        maxVol: 100,
-        loopMessage: false,
-        discordPlayer: {
-            ytdlOptions: {
-                quality: 'highestaudio',
-                highWaterMark: 1 << 25
-            }
-        }
-    },
-
     googlekeyV1: {
         googlekey: process.env.key
     },
